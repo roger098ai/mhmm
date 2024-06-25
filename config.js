ef014143b2584445aeabc9f7109b88039ab78299
roger098ai <159474000+roger098ai@users.noreply.github.com>
2024-06-25 14:27:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "for Saswoti",
+    introTitle: 'Saswoti',
+    introDesc: `They say absence makes the heart grow fonder, but in my case, it only makes me miss you more.
+     I wish I could hold your hand right now, but I can hold you in my thoughts and dreams. Maybe someday we can make our dreams a reality?`,
+    btnIntro: '^^HiHi^^',
+    title: 'Do I Make You Happy? 🥰',
+    desc: 'Did You Get Drunk on My Smile?',
+    btnYes: 'I Love It <33',
+    btnNo: 'Nope :3',
+    question: 'Among Over 8 Billion People in the World, thank you for loving me <3333',
+    btnReply: 'Send to You <3',
+    reply: 'I love you because you\'re the only one who makes my heart skip a beat, and with you, every moment feels like a dream come true.',
+    mess: 'I Know, Right? 🥰. I Love You So Much 😘😘',
+    messDesc: 'Tonight at 7:30pm, I\'ll Call You Up.',
+    btnAccept: 'Okay, I\'m In <3',
+    messLink: 'https://wa.me/qr/7RO72CTXX3WQK1' //link mess của các bạn. VD: https://wa.me/qr/7RO72CTXX3WQK1 
+}
